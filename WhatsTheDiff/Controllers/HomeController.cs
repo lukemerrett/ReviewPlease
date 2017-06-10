3fb65ba3819e0c12ac53bfb43d649f190ac7a21b
Luke Merrett <lukeamerrett@gmail.com>
2017-06-10 19:59:44 +0100

    Removed unneeded controller action

--- WhatsTheDiff/Controllers/HomeController.cs
+++ WhatsTheDiff/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace WhatsTheDiff.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
