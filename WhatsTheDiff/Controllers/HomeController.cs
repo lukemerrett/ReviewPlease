b3cd9c222e3d069afbb0583b8f4258b08a17f4d4
Luke Merrett <lukeamerrett@gmail.com>
2017-06-10 20:34:01 +0100

    Added basic for to get PR URL

--- WhatsTheDiff/Controllers/HomeController.cs
+++ WhatsTheDiff/Controllers/HomeController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatsTheDiff.Models;
 
 namespace WhatsTheDiff.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(new StartReviewModel());
         }
 
+        [HttpPost]
+        public ActionResult Index(StartReviewModel startReviewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get PR details
+            }
+
+            return View(startReviewModel);
+        }
+
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ WhatsTheDiff/Models/StartReviewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhatsTheDiff.Models
+{
+    public class StartReviewModel
+    {
+        [DisplayName("GitHub PR Url")]
+        [Required(ErrorMessage = "Please provide a PR link")]
+        public string GitPrUrl { get; set; }
+    }
+}
