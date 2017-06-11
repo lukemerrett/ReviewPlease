e9cadb54907d76ee67c18a5452ead58e5039f38f
Luke Merrett <lukeamerrett@gmail.com>
2017-06-11 15:42:52 +0100

    Scaffolding for parsing the URL

---
+++ Application/PRParser.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+
+namespace Application
+{
+    public static class PRParser
+    {
+        /// <summary>
+        /// Parses the provided url into the components needed to query the PR details.
+        /// </summary>
+        /// <param name="fullPrUrl">The full url to the PR, e.g: "https://github.com/{organisation}/{repository}/pull/{prId}"</param>
+        /// <returns>A DTO containing the details required to query the PR.</returns>
+        public static PrDetails Parse(string fullPrUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/PrDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace Models
+{
+    public class PrDetails
+    {
+        /// <summary>
+        /// Gets or sets the host. Usually github.com, however we want to cater for GitHub enterprise too.
+        /// </summary>
+        public string GithubHostName { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the name of the organisation the repository lives under.
+        /// </summary>
+        public string OrganisationName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the repository the PR has been raised in.
+        /// </summary>
+        public string RepositoryName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of the PR requested.
+        /// </summary>
+        public int PrNumber { get; set; }
+    }
+}
