683d279bf3de59631db42eb51e648706a2d55998
Luke Merrett <lukeamerrett@gmail.com>
2017-06-11 16:10:27 +0100

    Added wrapper for success/failure responses

---
+++ Application/OperationResult.cs
@@ -0,0 +1,34 @@
+﻿namespace Application
+{
+    public class OperationResult<SuccessType, FailureType>
+    {
+        private OperationResult()
+        {
+
+        }
+
+        public SuccessType Result { get; private set; }
+
+        public FailureType FailureReason { get; private set; }
+
+        public bool IsSuccess { get; private set; }
+
+        public static OperationResult<SuccessType, FailureType> Success(SuccessType result)
+        {
+            return new OperationResult<SuccessType, FailureType>
+            {
+                Result = result,
+                IsSuccess = true
+            };
+        }
+
+        public static OperationResult<SuccessType, FailureType> Failed(FailureType failureReason)
+        {
+            return new OperationResult<SuccessType, FailureType>
+            {
+                FailureReason = failureReason,
+                IsSuccess = false
+            };
+        }
+    }
+}
