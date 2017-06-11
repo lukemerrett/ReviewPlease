eeb4e80060002f78a01dee825e5b56af27a54cfe
Luke Merrett <lukeamerrett@gmail.com>
2017-06-11 15:53:02 +0100

    Logic to parse the url

--- Application/PRParser.cs
+++ Application/PRParser.cs
@@ -1,10 +1,15 @@
 ﻿using Models;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Application
 {
     public static class PRParser
     {
+        private static readonly Regex ParsingExpression = new Regex(
+            @"https?:\/\/(?<Host>.*?)\/(?<Organisation>.*?)\/(?<Repository>.*?)\/pull\/(?<Number>\d*)",
+            RegexOptions.Compiled);
+
         /// <summary>
         /// Parses the provided url into the components needed to query the PR details.
         /// </summary>
@@ -12,6 +17,18 @@ namespace Application
         /// <returns>A DTO containing the details required to query the PR.</returns>
         public static PrDetails Parse(string fullPrUrl)
         {
+            var match = ParsingExpression.Match(fullPrUrl);
+
+            if (match.Success)
+            {
+                return new PrDetails
+                {
+                    GithubHostName = match.Groups["Host"].Value,
+                    OrganisationName = match.Groups["Organisation"].Value,
+                    RepositoryName = match.Groups["Repository"].Value,
+                    PrNumber = int.Parse(match.Groups["Number"].Value)
+                };
+            }
             throw new NotImplementedException();
         }
     }
